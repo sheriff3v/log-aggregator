260744a0cad743371d5cd8634e8a1faa27e3a8e4
Valerii <vvoinarovych@gmail.com>
2025-03-31 21:49:43 +0200

    add docker-compose

---
+++ log-ingestor/src/main/java/com/vvoinarovych/logingestor/service/EventService.java
@@ -0,0 +1,7 @@
+package com.vvoinarovych.logingestor.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class EventService {
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+}
